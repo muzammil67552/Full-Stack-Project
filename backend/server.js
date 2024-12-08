042776016cbb89208d1b2ae51b99ef0d5dcb1455
Muhammad Muzammil <yoursmuzammil16070@gmail.com>
2024-12-08 15:03:16 +0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API is Running");
+});
+
+app.listen(4000, () => {
+    console.log("Server is running on http://localhost:4000");
+});
